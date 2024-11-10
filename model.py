d68c00fe8dcf7d01011cac2e153ed4ca458e73fe
Siddharth Mishra <120008126+Sid3503@users.noreply.github.com>
2024-11-10 21:46:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -178,7 +178,7 @@ class DecoderBlock(nn.Module):
 
     def forward(self, x, encoder_output, src_mask, tgt_mask):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
-        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))  # cross attention
         x = self.residual_connections[2](x, self.feed_forward_block)
         return x
 
